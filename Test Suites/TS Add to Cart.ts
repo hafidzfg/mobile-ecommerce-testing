403f4fb36d536158e4948950082172d57ff7e620
Hafidz Firmansyah Ghufara <hafidzfg@gmail.com>
2022-07-09 13:35:41 +0700

    issue Add to Cart Test Suites

---
+++ Test Suites/TS Add to Cart.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Kumpulan Test Case yang berhubungan dengan fungsi 'Cart'</description>
+   <name>TS Add to Cart</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>805914a8-c03a-4e5f-b549-072cb3263baf</testSuiteGuid>
+   <testCaseLink>
+      <guid>5e6e4081-2465-4131-8335-1735e5df3c8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1 Cart/TC 101_Add to cart with valid product quantity (Negative)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>385e23c0-e6ce-46f0-8620-a630733f322c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1 Cart/TC 102_Add to cart with 0 product quantity (Negative)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>70c56e54-096d-4473-aca1-0a21ae3a2fe5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1 Cart/TC 103_Add to cart with in valid product quantity (Positive)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
