db58f10952365d9a510b213b04f8cefe86453b15
Hafidz Firmansyah Ghufara <hafidzfg@gmail.com>
2022-07-09 17:54:50 +0700

    initial commit test suites profile

---
+++ Test Suites/TS Profile.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Sekumpulan Test Cases berfokus pada fungsionalitas fitur Profile</description>
+   <name>TS Profile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>97603d0c-ed2a-40d8-87d8-0d24af517c54</testSuiteGuid>
+   <testCaseLink>
+      <guid>526aa342-ca7b-4f80-93fe-5910de33c45d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3 Profile/TC 301_Edit Profile with Valid Details</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>85125dc6-a6f1-40e9-be5f-5ee7e9ec9396</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3 Profile/TC 302_Check Share Button</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7fd159fe-edcb-4d54-98d2-dfc87833d23b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3 Profile/TC 303_Check Rate Us Redirect</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>329c375b-b412-4dc7-8e54-05586c3dcf55</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3 Profile/TC 304_Check Privacy Policy Redirect</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
