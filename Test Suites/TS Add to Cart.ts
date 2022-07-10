4c4117bb2f00c977b33546b3ba34a14979f45467
Hafidz Firmansyah Ghufara <hafidzfg@gmail.com>
2022-07-10 11:39:59 +0700

    delete unused TC

--- Test Suites/TS Add to Cart.ts
+++ Test Suites/TS Add to Cart.ts
@@ -15,7 +15,7 @@
       <guid>5e6e4081-2465-4131-8335-1735e5df3c8a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 Cart/TC 101_Add to cart with valid product quantity (Negative)</testCaseId>
+      <testCaseId>Test Cases/1 Cart/TC 101_Add to cart with valid product quantity (Positive)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
@@ -29,7 +29,7 @@
       <guid>70c56e54-096d-4473-aca1-0a21ae3a2fe5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/1 Cart/TC 103_Add to cart with in valid product quantity (Positive)</testCaseId>
+      <testCaseId>Test Cases/1 Cart/TC 103_Add to cart with invalid product quantity (Negative)</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
